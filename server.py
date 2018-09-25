b238acf09b3d4992da5e38c4a3bfd0d53efac2d2
sahinyu <sahinyu@itu.edu.tr>
2018-09-26 01:20:28 +0300

    Add existing file

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+import views
+from database import Database
+from movie import Movie
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings")
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/movies", view_func=views.movies_page)
+    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
 
-app = Flask(__name__)
+    db = Database()
+    db.add_movie(Movie("Slaughterhouse-Five", year=1972))
+    db.add_movie(Movie("The Shining"))
+    app.config["db"] = db
 
-
-@app.route("/")
-def home_page():
-    return "Hello, world!"
-
+    return app
 
 if __name__ == "__main__":
-    app.run()
+    app = create_app()
+    port = app.config.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
---
+++ views.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask import current_app, render_template
+
+def home_page():
+    today = datetime.today()
+    day_name = today.strftime("%A")
+    return render_template("home.html", day=day_name)
+
+def movie_page(movie_key):
+    db = current_app.config["db"]
+    movie = db.get_movie(movie_key)
+    return render_template("movie.html", movie=movie)
+
+def movies_page():
+    db = current_app.config["db"]
+    movies = db.get_movies()
+
+    return render_template("movies.html", movies=sorted(movies))
